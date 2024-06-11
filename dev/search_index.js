52bacb2c2342ad14825ae667b323c04ab5f338a0
Documenter.jl <documenter@juliadocs.github.io>
2024-06-11 16:26:38 +0000

    build based on a65c141

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScaleBar","category":"page"},{"location":"#ScaleBar","page":"Home","title":"ScaleBar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScaleBar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScaleBar]","category":"page"}]
+}
