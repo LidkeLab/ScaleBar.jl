7d92b70f6143746044df05a20e7f4489850061a0
Ali <akazemins@unm.edu>
2024-06-24 15:36:38 -0600

    chore: Refactor get_bar_coordinates function to handle output_test_image correctly

--- dev/test.jl
+++ dev/test.jl
@@ -1,34 +1,21 @@
-
-function get_bar_coordinates(image::Matrix)
-    # Write your function here.
-    pos_x::Array = []
-    pos_y::Vector = []
-    ii = 0
-    for i in 1:size(output_test_image, 1)
-        for j in 1:size(output_test_image, 2)
-            if output_test_image[i, j] == 1
-                if ii == 0
-                    ii = 1
-                    pos_x = i
-                    pos_y = j
-                else
-                    ii += 1
-                    push!(pos_x, i)
-                    push!(pos_y, j)
-                end
+function get_bar_coordinates(image::Matrix{Int})
+    pos_x = Int[]
+    pos_y = Int[]
+    for i in 1:size(image, 1)
+        for j in 1:size(image, 2)
+            if image[i, j] == 1
+                push!(pos_x, i)
+                push!(pos_y, j)
             end
         end
     end
-    pos = zeros(length(pos_x), length(pos_y))
-    pos = [pos_x, pos_y]
+    pos = [pos_x pos_y]  # Create a 2-column matrix directly
     return pos
 end
 
+# Test scenario
+example_image = zeros(Int, 200, 100)  # Ensure the matrix is of integer type
+example_image[3:12, 6:15] .= 1  # Create a bar of 1s
+bar_coordinates = get_bar_coordinates(example_image)
 
-    # Write your tests here.
-
-    example_image = zeros(200, 100)
-    bar_size = 10
-    # output_test_image = scalebar(image::Array = example_image, bar_size::Int, orientation::String = "horizontal", position::String = "tl")
-    example_image[3:10, 6:36] .= 1
-    bar_coordinatres = get_bar_coordinates(example_image)
+println(bar_coordinates)  # This should print the coordinates of the bar
