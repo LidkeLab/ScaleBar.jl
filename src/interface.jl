e07cd275f34f4db6413e2aec5d6d0b7fb2a963eb
MahsaHabibi <mahsa.habibi16@gmail.com>
2024-06-24 15:30:34 -0600

    scslebar function

--- src/interface.jl
+++ src/interface.jl
@@ -1,30 +1,67 @@
 # main interface function:
+using Images
+using CairoMakie
 
+function scalebar!(img::AbstractArray, len::Real, pxsize::Float64, bar_pxsize::Float64, unit::String, position::String, orientation::String)
 
+    # unit (µm, nm, mm, cm)
+    # position (TopRight:TR, TopLeft:TL, BottomRight:BR, BottomLeft:BL)
+    # orientation (Horizontal:H, Vertical:V)
 
-function scalebar!(img::AbstractArray, length::Real, unit::String, position::String, orientaion::String)
+    img_size = (32, 32)
+#   img_size = (256, 256)
 
-    size(img) = (32, 32)
-    length = 40
-# unit (µm, nm, mm, cm)
-# position (TopRight:TR, TopLeft:TL, BottomRight:BR, BottomLeft:BL)
-# orientation (Horizontal:H, Vertical:V)
+    len = 40
+
+    pxsize = 0.1
+
+    bar_pxsize = len / pxsize
+
 
     # Get the size of the image
     img_sizex, img_sizey = size(img)
+
+
+
+
+    x1, y1, x2, y2 = 0, 0, 0, 0
     
-    
-    # Get the position of the scalebar
-    x, y = position
-    
-    # Draw the scalebar
-    # for i in 1:scalebar_height
-    #     for j in 1:scalebar_width
-    #         img[Int(round(y + i)), Int(round(x + j))] = color
-    #     end
-    # end
-    
-    # Draw the text
-    #text!(img, "$scale $unit", (x + scalebar_width + 5, y + scalebar_height), color=color, fontsize=10) 
+    if position in ["TopRight", "TR"]
+        if orientation == "Horizontal"
+            x1, y1 = img_sizey - margin - length, margin
+            x2, y2 = img_sizey - margin, margin
+        else
+            x1, y1 = img_sizey - margin, margin
+            x2, y2 = img_sizey - margin, margin + length
+        end
+    elseif position in ["TopLeft", "TL"]
+        if orientation == "Horizontal"
+            x1, y1 = margin, margin
+            x2, y2 = margin + length, margin
+        else
+            x1, y1 = margin, margin
+            x2, y2 = margin, margin + length
+        end
+    elseif position in ["BottomRight", "BR"]
+        if orientation == "Horizontal"
+            x1, y1 = img_sizey - margin - length, img_sizex - margin
+            x2, y2 = img_sizey - margin, img_sizex - margin
+        else
+            x1, y1 = img_sizey - margin, img_sizex - margin - length
+            x2, y2 = img_sizey - margin, img_sizex - margin
+        end
+    elseif position in ["BottomLeft", "BL"]
+        if orientation == "Horizontal"
+            x1, y1 = margin, img_sizex - margin
+            x2, y2 = margin + length, img_sizex - margin
+        else
+            x1, y1 = margin, img_sizex - margin - length
+            x2, y2 = margin, img_sizex - margin
+        end
+    else
+        error("Invalid position value")
+    end
+
+
 
 end
