def950f99a7b5be425bb2da4285d9b26175b1199
Ali <akazemins@unm.edu>
2024-06-24 10:43:14 -0600

    chore: Add HDF5 dependency and test for scalebar function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using ScaleBar
 using Test
+using HDF5
 
 @testset "ScaleBar.jl" begin
     # Write your tests here.
+
+    example image = zeros(200, 100)
+    bar_size = [10, 20]
+    output_test_image = scalebar(bar_size, "horizontal", "tl", "mm")
+    @testset "Test scalebar" begin
+        @test output_test_image == example image
+    end
+
+   
 end
