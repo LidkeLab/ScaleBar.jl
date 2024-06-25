220beb216c0b2951acaba5761006ce1c7d29b5ab
MahsaHabibi <mahsa.habibi16@gmail.com>
2024-06-25 08:26:51 -0600

    function scalebar

--- src/interface.jl
+++ src/interface.jl
@@ -4,76 +4,46 @@ using CairoMakie
 
 img = load("/Users/Mahsa/Desktop/The_famous_Lena.png")
 
-# img_size = (32, 32)
-# img_size = (256, 256)
-
 length = 10
-
 pxsize = 0.1
 
-
 x, y = size(img)
-   
 img_sizex = x
 img_sizey = y
-    
+
 bar_pxsize = length / pxsize
 bar_thickness = max(1, min(img_sizex, img_sizey) ÷ 20)
-margin = 2 * bar_thickness
 bar_middle = bar_thickness ÷ 2
 
 position = "TR"
 orientation = "Horizontal"
 
-function scalebar!(img::AbstractArray, length::Real, pxsize::Float64, bar_pxsize::Float64, unit::String, position::String, orientation::String)
-
-    # unit (µm, nm, mm, cm)
-    # position (TopRight:TR, TopLeft:TL, BottomRight:BR, BottomLeft:BL)
-    # orientation (Horizontal:H, Vertical:V)
-    
+function scalebar!(img::AbstractArray, length::Real, pxsize::Float64, bar_pxsize::Float64, bar_thickness, position::String, orientation::String)
     img_sizey, img_sizex = size(img)
+    ar_pxsize = length / pxsize
     bar_thickness = max(1, min(img_sizex, img_sizey) ÷ 20)
-    margin = 2 * bar_thickness
 
+    println("Image size: ", (img_sizex, img_sizey))
+    println("Bar thickness: ", bar_thickness)
+    println("Bar pixel size: ", bar_pxsize)
 
     if position == "TR"
         if orientation == "Horizontal"
-            for idx in img_sizey - margin - length: img_sizey - margin
-                img[idx, :] .= 1
+            start_col = Int(img_sizey - bar_pxsize - bar_thickness)
+            end_col = Int(img_sizey - bar_pxsize)
+
+            println("Start column: ", start_col)
+            println("End column: ", end_col)
+
+            for idx in start_col:end_col
+                img[1:Int(bar_thickness), idx] .= RGB(1, 1, 1)
             end
-        # else
-        #     x1, y1 = img_sizey - margin, margin
-        #     x2, y2 = img_sizey - margin, margin + length
         end
-    # elseif position == "TL"
-    #     if orientation == "Horizontal"
-    #         x1, y1 = margin, margin
-    #         x2, y2 = margin + length, margin
-    #     else
-    #         x1, y1 = margin, margin
-    #         x2, y2 = margin, margin + length
-    #     end
-    # elseif position in "BR"
-    #     if orientation == "Horizontal"
-    #         x1, y1 = img_sizey - margin - length, img_sizex - margin
-    #         x2, y2 = img_sizey - margin, img_sizex - margin
-    #     else
-    #         x1, y1 = img_sizey - margin, img_sizex - margin - length
-    #         x2, y2 = img_sizey - margin, img_sizex - margin
-    #     end
-    # elseif position in "BL"
-    #     if orientation == "Horizontal"
-    #         x1, y1 = margin, img_sizex - margin
-    #         x2, y2 = margin + length, img_sizex - margin
-    #     else
-    #         x1, y1 = margin, img_sizex - margin - length
-    #         x2, y2 = margin, img_sizex - margin
-    #     end
-    # else
-    #     error("Invalid position value")
-
     end
-
 end
+
+scalebar!(img, length, pxsize, bar_pxsize, bar_thickness, position, orientation)
+
+save("/Users/Mahsa/Desktop/The_famous_Lena_with_scalebar.png", img)
 
 display(img)
