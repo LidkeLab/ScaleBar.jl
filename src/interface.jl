fd8f3668574179ce740bc13438bb04f4145875f5
MahsaHabibi <mahsa.habibi16@gmail.com>
2024-06-25 07:41:07 -0600

    scalebar function

--- src/interface.jl
+++ src/interface.jl
@@ -2,41 +2,45 @@
 using Images
 using CairoMakie
 
+img = load("/Users/Mahsa/Desktop/The_famous_Lena.png")
+
+# img_size = (32, 32)
+# img_size = (256, 256)
+
+length = 10
+
+pxsize = 0.1
+
+
+x, y = size(img)
+   
+img_sizex = x
+img_sizey = y
+    
+bar_pxsize = length / pxsize
+bar_thickness = max(1, min(img_sizex, img_sizey) ÷ 20)
+margin = 2 * bar_thickness
+bar_middle = bar_thickness ÷ 2
+
+position = "TR"
+orientation = "Horizontal"
+
 function scalebar!(img::AbstractArray, length::Real, pxsize::Float64, bar_pxsize::Float64, unit::String, position::String, orientation::String)
 
     # unit (µm, nm, mm, cm)
     # position (TopRight:TR, TopLeft:TL, BottomRight:BR, BottomLeft:BL)
     # orientation (Horizontal:H, Vertical:V)
-
-#   img_size = (32, 32)
-#   img_size = (256, 256)
-
-#   length = 40
-
-    pxsize = 0.1
-
-    # Get the size of the image
-    x, y = size(img)
-   
-    img_sizex = x
-    img_sizey = y
-    
-    bar_pxsize = len / pxsize
-    bar_thickness = max(1, min(img_sizex, img_sizey) ÷ 20)
-    margin = 2 * bar_thickness
-    bar_middle = bar_thickness ÷ 2
-    
     
     if position == "TR"
         if orientation == "Horizontal"
             for idx in img_sizey - margin - length: img_sizey - margin
-                img[idx] .= 1
+                img[idx, :] .= 1
             end
         else
             x1, y1 = img_sizey - margin, margin
             x2, y2 = img_sizey - margin, margin + length
         end
-    elseif position = "TL"
+    elseif position == "TL"
         if orientation == "Horizontal"
             x1, y1 = margin, margin
             x2, y2 = margin + length, margin
@@ -63,8 +67,11 @@ function scalebar!(img::AbstractArray, length::Real, pxsize::Float64, bar_pxsize
     else
         error("Invalid position value")
 
-        
     end
+
+end
+
+display(img)
 
 
 
