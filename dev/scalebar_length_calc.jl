6442496822163e848f99d5fb5bcfa651855d6810
IMAdams <ian.marshall.adams@gmail.com>
2024-06-28 11:24:03 -0600

    scalebar length calc draft added

---
+++ dev/scalebar_length_calc.jl
@@ -0,0 +1,36 @@
+""" 
+in development
+    len_calc(img::AbstractArray)
+
+Determine default scalebar length, in pixels, based on the dimensions of the input image. 
+
+# Arguments
+    img::AbstractArray - input array containing image data
+# Returns
+    the dimensions of a rectangle with side lengths 20% the length of the image.
+
+
+""" 
+
+using Distributions
+using Revise
+includet("simDATA2ima.jl")
+
+test_im = gen_points(;)
+
+function len_calc(img::AbstractArray)
+   
+    # get the dimensions of the input image
+    fx  = size(img)
+
+    # find 20% the length of each dimension
+    smfx = 0.2 .* fx
+    
+    # round these values to one of a set of easy-to-read values. 
+    sb_dims = round(smfx, ???)
+    return sb_dims
+
+end
+
+
+b = len_calc(test_im)
