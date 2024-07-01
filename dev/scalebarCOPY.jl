1c8a84bcbd1def659dac2e5eb20915537c2ca470
IMAdams <ian.marshall.adams@gmail.com>
2024-07-01 01:28:25 -0600

    Improved scalebar_length_calc and integrated with scalebar function

--- dev/scalebarCOPY.jl
+++ dev/scalebarCOPY.jl
@@ -1,105 +1,47 @@
-# copied from scalebar_imp dev branch on 6-29.
+"""
+in developemt - I integrated scalebar_length_calc.jl
+    scalebar!(img::AbstractArray; 
+        position::String, 
+        pxsize::F, 
+        len::Real, 
+        scale::Int,
+        units::String,
+        width::Real  
+     ) 
+
+Create a scalebar for an input image and apply that scalebar to the image. 
+
+# Arguments
+    img::AbstractArray         : A 2-dimensional array of pixels
+    position::String           : "br" = bottom right, "ul" = upper left, etc
+    pxsize::Real               : pixel size
+    len::Real                  : the scalebar length, in pixels. The default is determined by the "scalebar_length_calc.jl" function len_calc() 
+    scale::Int                 : scaling factor
+    units::String              : units 
+    width::Real                : similar to length. 
+
+# Returns
+
+"""
+
+
 using Revise
 using Colors
 using Images
 using CairoMakie
-using GeometryBasics
-#=
-function scalebar!(img::AbstractArray, scale::Real, unit::String, position::Tuple{Real,Real}, color::RGB{N0f8}=RGB{N0f8}(1,1,1))
-    # Get the size of the image
-    height, width = size(img)
-    
-    # Get the size of the scalebar
-    scalebar_width = 100
-    scalebar_height = 10
-    
-    # Get the position of the scalebar
-    x, y = position
-    
-    # Draw the scalebar
-    for i in 1:scalebar_height
-        for j in 1:scalebar_width
-            img[Int(round(y + i)), Int(round(x + j))] = color
-        end
-    end
-    
-    # Draw the text
-    #text!(img, "$scale $unit", (x + scalebar_width + 5, y + scalebar_height), color=color, fontsize=10) 
 
-end =#
-
-## function method for bar and whiskers scalebar. 
-# function scalebar!(img::AbstractArray; 
-#     position::String = "br", 
-#     pxsize::Float64 = 0.5, 
-#     len::Real = 20, 
-#     scale::Int=15,
-#     units::String = "nm" )
-    
-#     img_sizex = size(img,2)
-#     img_sizey = size(img,1)
-#     len_bar = round(Int,len/pxsize)
-#     width_bar = round(Int,len_bar/(scale/2))
-#     offset_x = round(Int,img_sizex/scale)
-#     offset_y = round(Int,img_sizey/scale)
-#     if position[1] == 'b'
-#         x_i = img_sizey-width_bar-offset_y
-#         x_f = img_sizey-offset_y
-#         println("b")
-#     elseif position[1] == 'u'
-#         x_i = offset_y
-#         x_f = offset_y+width_bar
-#         println("u")
-#     end
-#     if position[2] == 'r'
-#         y_i = img_sizex-len_bar-offset_x
-#         y_f = img_sizex-offset_x
-#         println("r")
-#     elseif position[2] == 'l'
-#         y_i = offset_x
-#         y_f = offset_x+len_bar 
-#         println("l")
-#     end
-#     return x_i, x_f, y_i, y_f
-# end   
-
-##demostration scale bar
-
-## method for a rectangular scalebar
-function scalebar(img::AbstractArray; 
-    position::String = "br", 
-    pxsize::Float64 = 0.5, 
-    len::Real = 20, 
-    scale::Int=15,
-    units::String = "nm" )
- img_new = deepcopy(img)
- scalebar!(img_new,position,pxsize,len,scale)
- return img_new 
-end
-#draw the scale bar 
-function scalebar_draw(img::AbstractArray,x_i::Int64, x_f::Int64, y_i::Int64, y_f::Int64)
-    midle_bar=round(Int,((x_f-x_i)/2)+x_i)
-    img[x_i:x_f,y_i-1].= RGB(0,0,0)
-    img[x_i:x_f,y_f+1].= RGB(0,0,0)
-    img[midle_bar,y_i:y_f].= RGB(0,0,0)
-end
-
-#test scale bar 
-img = RGB.(ones(512,512))
-x_i, x_f, y_i, y_f=scalebar!(img,position="br",len=50,)
-scalebar_draw(img,x_i, x_f, y_i, y_f)
-img
-
-
+includet("simDATA2ima.jl")
+includet("scalebar_length_calc.jl")
 
 # Modified ver to add width parameter
 function scalebar!(img::AbstractArray; 
     position::String = "br", 
     pxsize::Float64 = 0.5, 
-    len::Real = 20, 
+    len::Real = len_calc(img)[1], 
     scale::Int=15,
     units::String = "nm",
-    width::Real = 5 ) # Added width parameter
+    width::Real = len_calc(img)[2]
+     )
     
     img_sizex = size(img,2)
     img_sizey = size(img,1)
@@ -131,10 +73,11 @@ end
 function scalebar(img::AbstractArray; 
     position::String = "br", 
     pxsize::Float64 = 0.5, 
-    len::Real = 20, 
+    len::Real = len_calc(img)[1], 
     scale::Int=15,
     units::String = "nm",
-    width::Real = 5 ) # Added width parameter
+    width::Real = len_calc(img)[2]
+     )
     
  img_new = deepcopy(img)
  scalebar!(img_new,position=position,pxsize=pxsize,len=len,scale=scale,width=width) # Added width parameter
@@ -142,13 +85,13 @@ function scalebar(img::AbstractArray;
 end
 
 #draw the scale bar with width
-function scalebar_draw(img::AbstractArray,x_i::Int64, x_f::Int64, y_i::Int64, y_f::Int64)
+function scalebar_draw(img::Union{AbstractArray, Matrix{UInt16}},x_i::Int64, x_f::Int64, y_i::Int64, y_f::Int64)
     img[x_i:x_f, y_i:y_f] .= RGB(0,0,0) # Fill in the rectangle
 end
 
 #test scale bar 
-img = RGB.(ones(512,512))
-x_i, x_f, y_i, y_f = scalebar!(img, position="br", len=50, width=10)
+img = RGB.(ones(1028,1920))
+x_i, x_f, y_i, y_f = scalebar!(img, position="br")
 scalebar_draw(img, x_i, x_f, y_i, y_f)
 img
 
--- dev/scalebar_length_calc.jl
+++ dev/scalebar_length_calc.jl
@@ -5,36 +5,44 @@ in development
 Determine default scalebar length, in pixels, based on the dimensions of the input image. 
 
 # Arguments
-    img::AbstractArray - input array containing image data
+    img::AbstractArray : input array containing image data
 # Returns
-    the dimensions of a rectangle with side lengths 20% the length of the image.
+    len : scalebar length dimension in pixels
+    width : scalebar width dimension in pixels
 
 
 """ 
 
 using Distributions
 using Revise
-includet("simDATA2ima.jl")
 
-test_im = gen_points(;)
-
-function len_calc(img::AbstractArray)
+function len_calc(img::Union{AbstractArray, Array{RGB{Float64}}})
    
     # get the dimensions of the input image
-    fx  = size(img)
-
-    # find 20% the length of each dimension
-    smfx = 0.2 .* fx
+    len_dim = size(img)[2]
     
-    # round these values to one of a set of easy-to-read values. 
-    sb_dims = (round(smfx[1]),round(smfx[2]))
-             ## need a way to find the nearest multiple that will work for a scalebar.
-             ## to avoid wierd lengths like "95" it might be nice to restrict the options a little bit. some thing like a set of reasonable options, like [1,2,5,10,20,25,50,75,100, 150, 200]
-    return sb_dims
+    # find 20% the length of the image
+    len_sb = 0.2 .* len_dim
+    
+    # find the nearest multiple of 5. May be nice to add more detail here, for example, a case for small images with scalebar lengths of 1 or 2. for larger input dimensions it will be nice to have different rules for larger images that will have scalebar length in the hundreds. 
+    if len_sb > 150
+        sb_len = (len_sb +(100-len_sb%100))
+    else 
+        sb_len = (len_sb-len_sb%5)
+    end
+    
+
+    # set the width based on the length 
+    sb_wid = sb_len*.25
+    sb_wid = sb_wid - sb_wid %5
+    sb_dims = (sb_len, sb_wid)
+    
+    println("default scalebar dimensions calculated:", sb_dims) # this prints twice
+        
+    len = convert(Int, sb_len)
+    width = convert(Int, sb_wid)
+    return len, width
 
 end
 
-
-
-b = len_calc(test_im)
 
