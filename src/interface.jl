cf86e6b3fef4b95387a5cd1fc23ccd5b34fd171a
parzival1401 <fernandodelgu14@unm.edu>
2024-07-17 14:14:54 -0600

    fix the symbol issue and correct the offset, adding the fucntionality for changing the offset in x and y independently

--- src/interface.jl
+++ src/interface.jl
@@ -25,10 +25,11 @@ This is the major interface function for the package
 """
 function scalebar!(img::AbstractArray, # updated function sigature with len_calc (-Ian)
     pxsize::Float64; 
-    position::String = "br", 
+    position::Symbol = :br, 
     len::Real = len_calc(img)[1],    # length and width default to results of len_calc()
     width::Real = len_calc(img)[2],
-    scale::Int=15,
+    offsetx::Int=30,
+    offsety::Int=30,
     color::Symbol= :white ) # Added width parameter
     
     img_sizex = size(img,2)
@@ -36,26 +37,34 @@ function scalebar!(img::AbstractArray, # updated function sigature with len_calc
     len_bar = round(Int,len/pxsize)
     println("len_bar: ",len_bar)
     width_bar = round(Int,width/pxsize) # Use width parameter to set width_bar
-    offset_x = round(Int,img_sizex/scale)
-    offset_y = round(Int,img_sizey/scale)
-    if position[1] == 'b'
+    offset_x = round(Int,img_sizex/offsetx)
+    offset_y = round(Int,img_sizey/offsety)
+    if position== :br
         x_i = img_sizey - width_bar - offset_y + 1
         x_f = img_sizey - offset_y
-        #println("b")
-    elseif position[1] == 't'
-        x_i = offset_y
-        x_f = offset_y + width_bar - 1
-        #println("t")
-    end
-    if position[2] == 'r'
         y_i = img_sizex - len_bar - offset_x + 1
         y_f = img_sizex - offset_x
-        #println("r")
-    elseif position[2] == 'l'
+        #println("b")
+    elseif position == :bl
+        x_i = img_sizey - width_bar - offset_y + 1
+        x_f = img_sizey - offset_y
         y_i = offset_x
         y_f = offset_x + len_bar - 1
-        #println("l")
+        #println("bl")
+    elseif position == :tl
+        x_i = offset_y
+        x_f = offset_y + width_bar - 1
+        y_i = offset_x
+        y_f = offset_x + len_bar - 1
+        #println("ul")
+    elseif position == :tr
+        x_i = offset_y
+        x_f = offset_y + width_bar - 1
+        y_i = img_sizex - len_bar - offset_x + 1
+        y_f = img_sizex - offset_x
+        #println("ur")
     end
+   
     if color == :white
         return img[x_i:x_f, y_i:y_f] .= RGB(1,1,1) # Fill in the rectangle
     elseif color == :black
@@ -91,14 +100,15 @@ Copy img and pass the copy to scalebar!()
 """
 function scalebar(img::AbstractArray, # updated function sigature with len_calc (-Ian)
     pxsize::Float64; 
-    position::String = "br", 
+    position::Symbol= :br, 
     len::Real = len_calc(img)[1],    # length and width default to results of len_calc()
     width::Real = len_calc(img)[2],
-    scale::Int=15,
+    offsetx::Int=30,
+    offsety::Int=30,
     color::Symbol= :white )  # Added width parameter
     
  img_new = deepcopy(img)
- scalebar!(img_new,pxsize,position=position,len=len,scale=scale,width=width,color=color) # Added width parameter
+ scalebar!(img_new,pxsize,position=position,len=len,offsetx=offsetx,offsety=offsety,width=width,color=color) # Added width parameter
  return img_new 
 end
 
@@ -122,7 +132,7 @@ function len_calc(img::Union{AbstractArray, Array{Float64}})
     len_dim = size(img)[2]
     
     # find 20% the length of the image
-    len_sb = 0.2 .* len_dim
+    len_sb = 0.1 .* len_dim
     
     # find the nearest multiple of 5
     if len_sb > 150
@@ -133,6 +143,7 @@ function len_calc(img::Union{AbstractArray, Array{Float64}})
     
 
     # set the width based on the length 
+    
     sb_wid = sb_len*.25
     sb_wid = sb_wid - sb_wid %5
     sb_dims = (sb_len, sb_wid)
@@ -140,13 +151,15 @@ function len_calc(img::Union{AbstractArray, Array{Float64}})
     println("default scalebar dimensions calculated:", sb_dims)
         
     len = convert(Int, sb_len)
+    println("len: ",len)
     width = convert(Int, sb_wid)
+    println("width: ",width)
     return len, width
 
 end
 
 # # test scale bar 
-# img = RGB.(ones(512,512))
-# scalebar!(img,0.5,color=:black)
-# img
+#img = RGB.(ones(512,512))
+#scalebar!(img,0.5,color=:black)
+#img
 
